63d1398073d48bd8655bb4d2b2bd6cc5ee097fd8
XU XILIE <xuxilie@cgpa8.comp.nus.edu.sg>
2022-04-03 19:04:11 +0800

    run_shell

---
+++ run.sh
@@ -0,0 +1,83 @@
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=1 
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=2 --method='Random' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=4 --method='CRAIG' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=0
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=1
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=2
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=4
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
+
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='mrpc' --fraction=1 
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='mrpc' --fraction=0.1 --ss=2 --method='Random' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='mrpc' --fraction=0.1 --ss=4 --method='CRAIG' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='mrpc' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=0
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='mrpc' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=1
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='mrpc' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=2
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='mrpc' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='mrpc' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=4
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='mrpc' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
+
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qqp' --fraction=1 
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qqp' --fraction=0.1 --ss=2 --method='Random' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qqp' --fraction=0.1 --ss=4 --method='CRAIG' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qqp' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=0
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qqp' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=1
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qqp' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=2
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qqp' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qqp' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=4
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qqp' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
+
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='rte' --fraction=1 
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='rte' --fraction=0.1 --ss=2 --method='Random' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='rte' --fraction=0.1 --ss=4 --method='CRAIG' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='rte' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=0
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='rte' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=1
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='rte' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=2
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='rte' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='rte' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=4
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='rte' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
+
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qnli' --fraction=1 
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qnli' --fraction=0.1 --ss=2 --method='Random' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qnli' --fraction=0.1 --ss=4 --method='CRAIG' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=0
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=1
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=2
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=4
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='qnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
+
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='wnli' --fraction=1 
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='wnli' --fraction=0.1 --ss=2 --method='Random' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='wnli' --fraction=0.1 --ss=4 --method='CRAIG' --select_every=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='wnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=0
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='wnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=1
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='wnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=2
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='wnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=3
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='wnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=4
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='wnli' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
+
+
+
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.2 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.3 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.5 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.8 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
+
+
+python train.py --gpu=0 --lr=0.0005 --num_layers=1 --max_len=36 --dataset='sst2' --fraction=1 --model_name='LSTM'
+python train.py --gpu=0 --lr=0.0005 --num_layers=1 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5 --model_name='LSTM'
+
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=1 --model_name='LSTM'
+python train.py --gpu=0 --lr=0.0005 --num_layers=2 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5 --model_name='LSTM'
+
+python train.py --gpu=0 --lr=0.0005 --num_layers=3 --max_len=36 --dataset='sst2' --fraction=1 --model_name='LSTM'
+python train.py --gpu=0 --lr=0.0005 --num_layers=3 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5 --model_name='LSTM'
+
+
+python train.py --gpu=0 --lr=0.0005 --num_layers=1 --max_len=36 --dataset='sst2' --fraction=1
+python train.py --gpu=0 --lr=0.0005 --num_layers=1 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
+
+python train.py --gpu=0 --lr=0.0005 --num_layers=3 --max_len=36 --dataset='sst2' --fraction=1
+python train.py --gpu=0 --lr=0.0005 --num_layers=3 --max_len=36 --dataset='sst2' --fraction=0.1 --ss=3  --method='GLISTERD' --select_every=3 --emb_weight=5
